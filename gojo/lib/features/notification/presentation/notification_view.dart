58f8e921ef13f8b08847e4e6f9aff9f37e85af50
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-03-30 21:46:21 +0300

    Add notification screen

---
+++ gojo/lib/features/notification/presentation/notification_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:gojo/Gojo-Mobile-Shared/UI/widgets/parent_view.dart';
+import 'package:gojo/Gojo-Mobile-Shared/UI/list_items/gojo_notifcation_item.dart';
+
+class NotificationView extends StatelessWidget {
+  const NotificationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GojoParentView(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            GojoNotificationItem(
+              isAppointment: false,
+              title: "Nabek Abebe",
+              subtitle: "Withdrew 10,0000 Birrs",
+            ),
+            GojoNotificationItem(
+              isAppointment: true,
+              title: "Kidus Yoseph",
+              subtitle: "Scheduled an appointment for Mar 27 at 8:00 AM",
+            ),
+            GojoNotificationItem(
+              isAppointment: false,
+              title: "Lingerew Getie",
+              subtitle: "Applied for a rent",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
