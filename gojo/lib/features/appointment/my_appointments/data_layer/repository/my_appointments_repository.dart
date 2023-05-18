a225fb80b7fcbe6b318698b0dd13b25c1b427521
Kidus <se.kidus.yoseph@gmail.com>
2023-05-18 18:32:31 +0300

    Add my appointments datalayer

---
+++ gojo/lib/features/appointment/my_appointments/data_layer/repository/my_appointments_repository.dart
@@ -0,0 +1,62 @@
+import 'package:gojo/features/appointment/my_appointments/data_layer/model/appointment.dart';
+import 'package:gojo/features/appointment/my_appointments/data_layer/repository/my_appointments_client.dart';
+
+abstract class MyAppointmentsRepositoryAPI {
+  Future<List<Appointment>> getMyAppointments();
+  Future<void> cancelAppointment(String id);
+}
+
+class MyAppointmentsRepositoryImpl implements MyAppointmentsRepositoryAPI {
+  MyAppointmentsClientAPI myAppointmentsClientAPI;
+
+  MyAppointmentsRepositoryImpl(this.myAppointmentsClientAPI);
+
+  @override
+  Future<List<Appointment>> getMyAppointments() async {
+    return await myAppointmentsClientAPI.getMyAppointments();
+  }
+
+  @override
+  Future<void> cancelAppointment(String id) async {
+    myAppointmentsClientAPI.cancelAppointment(id);
+  }
+}
+
+class MyAppointmentsRepositoryFake implements MyAppointmentsRepositoryAPI {
+  final fakeAppointments = [
+    Appointment(
+      phoneNumber: "0911234532",
+      id: "1",
+      fullName: "Braha Marlam Roh",
+      date: "March 21, 2023",
+    ),
+    Appointment(
+      phoneNumber: "0911234532",
+      id: "2",
+      fullName: "Braha Marlam Roh",
+      date: "March 21, 2023",
+    ),
+    Appointment(
+      phoneNumber: "0911234532",
+      id: "3",
+      fullName: "Braha Marlam Roh",
+      date: "March 21, 2023",
+    ),
+    Appointment(
+      phoneNumber: "0911234532",
+      id: "4",
+      fullName: "Braha Marlam Roh",
+      date: "March 21, 2023",
+    ),
+  ];
+
+  @override
+  Future<List<Appointment>> getMyAppointments() {
+    return Future.delayed(const Duration(seconds: 1), () => fakeAppointments);
+  }
+
+  @override
+  Future<void> cancelAppointment(String id) async {
+    fakeAppointments.removeWhere((element) => element.id == id);
+  }
+}
