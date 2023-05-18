fdd283b95752e2531a2c3e6acf1c055452468de1
Kidus <se.kidus.yoseph@gmail.com>
2023-05-18 16:34:02 +0300

    Refactor MyAppointmentsView

--- gojo/lib/features/appointment/my_appointments/presentation/my_appointment_view.dart
+++ gojo/lib/features/appointment/my_appointments/presentation/my_appointment_view.dart
@@ -1,5 +1,6 @@
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:gojo/Gojo-Mobile-Shared/UI/design_tokens/padding.dart';
 import 'package:gojo/Gojo-Mobile-Shared/UI/widgets/bar_button.dart';
 import 'package:gojo/Gojo-Mobile-Shared/UI/widgets/parent_view.dart';
 
@@ -14,25 +15,23 @@ class MyAppointmentsView extends StatelessWidget {
           slivers: [
             SliverList(
               delegate: SliverChildListDelegate(
-                [_buildHeader(context)],
+                const [_Header()],
                 addRepaintBoundaries: false,
               ),
             ),
             SliverFillRemaining(
               hasScrollBody: true,
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+              child: Padding(
+                padding: const EdgeInsets.all(GojoPadding.large),
+                child: SingleChildScrollView(
                   child: Column(
                     children: List.filled(
-                      4,
-                      Column(
-                        children: [
-                          _buildExpandableView(context),
-                          const Divider(),
-                        ],
-                      ),
-                    ),
+                        5,
+                        const _ExpandableView(
+                          fullName: "Braha Marlam Roh",
+                          date: "Jan 2, 2023",
+                          phoneNumber: "0955211643",
+                        )),
                   ),
                 ),
               ),
@@ -40,8 +39,15 @@ class MyAppointmentsView extends StatelessWidget {
           ],
         ));
   }
+}
 
-  Column _buildHeader(BuildContext context) {
+class _Header extends StatelessWidget {
+  const _Header({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
         const SizedBox(height: 10),
@@ -66,36 +72,55 @@ class MyAppointmentsView extends StatelessWidget {
       ],
     );
   }
+}
 
-  Widget _buildExpandableView(BuildContext context) {
-    return ExpandablePanel(
-      theme: const ExpandableThemeData(
-        headerAlignment: ExpandablePanelHeaderAlignment.center,
-      ),
-      header: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [
-          Text("Braha Marlam Roh"),
-          Text("Jan 2, 20223"),
-        ],
-      ),
-      collapsed: Container(),
-      expanded: Container(
-        padding: const EdgeInsets.only(left: 26),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Phone: 0955211643"),
-            const SizedBox(height: 15),
-            GojoBarButton(
-              customHeight: 40,
-              title: "Withdraw Appointment",
-              onClick: () {},
+class _ExpandableView extends StatelessWidget {
+  final String fullName;
+  final String date;
+  final String phoneNumber;
+
+  const _ExpandableView({
+    super.key,
+    required this.fullName,
+    required this.date,
+    required this.phoneNumber,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ExpandablePanel(
+          theme: const ExpandableThemeData(
+            headerAlignment: ExpandablePanelHeaderAlignment.center,
+          ),
+          header: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(fullName),
+              Text(date),
+            ],
+          ),
+          collapsed: Container(),
+          expanded: Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(phoneNumber),
+                const SizedBox(height: 15),
+                GojoBarButton(
+                  customHeight: 40,
+                  title: "Withdraw Appointment",
+                  onClick: () {},
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+        const Divider(),
+      ],
     );
   }
 }
