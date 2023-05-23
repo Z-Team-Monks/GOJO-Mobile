30a5a95fdcce240e73bbd85167364e0a39fd97c7
Kidus <se.kidus.yoseph@gmail.com>
2023-05-24 00:52:49 +0300

    Move from bloc listner to builder for route guard

--- gojo/lib/app.dart
+++ gojo/lib/app.dart
@@ -1,39 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gojo/Gojo-Mobile-Shared/UI/widgets/splash.dart';
-import 'package:gojo/constants/strings/app_routes.dart';
+import 'package:gojo/features/auth/signin/presentation/screen/signin_view.dart';
 import 'package:gojo/features/route_guard/presentation/bloc/route_guard_bloc.dart';
+import 'package:gojo/root.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<RouteGuardBloc, RouteGuardState>(
-      listener: (context, state) {
+    return BlocBuilder<RouteGuardBloc, RouteGuardState>(
+      builder: (context, state) {
         switch (state) {
           case RouteGuardState.authenticated:
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              GojoRoutes.root,
-              (Route<dynamic> route) => false,
-            );
-            break;
+            return const RootView();
           case RouteGuardState.unauthenticated:
-            Navigator.of(context).pushNamedAndRemoveUntil(
-              GojoRoutes.signin,
-              (Route<dynamic> route) => false,
-            );
-            break;
+            return const SignInView();
           case RouteGuardState.loading:
-            break;
+            return const GojoSplash();
         }
-      },
-      builder: (context, state) {
-        return Center(child: GojoSplash(
-          nextScreen: () async {
-            return await Container();
-          },
-        ));
       },
     );
   }
