c496c4d7a70f188fe50384beb8aecc1a051c148d
Kidus <se.kidus.yoseph@gmail.com>
2023-05-23 18:21:09 +0300

    Decide initial route based on route_guard bloc

--- gojo/lib/main.dart
+++ gojo/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:gojo/Gojo-Mobile-Shared/resources/resources.dart';
+import 'package:gojo/core/repository/user_repository.dart';
+import 'package:gojo/features/route_guard/presentation/bloc/route_guard_bloc.dart';
 import 'package:gojo/locator.dart';
 import 'package:gojo/navigation/route_generator.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,15 +24,22 @@ void main() async {
 
   runApp(
     OverlaySupport(
-      child: MaterialApp(
-        title: 'Gojo',
-        theme: ThemeData(
-          fontFamily: GoogleFonts.nunito().fontFamily,
-          primarySwatch:
-              Resources.gojoColors.primaryMaterialColor.materialColor,
+      child: BlocProvider(
+        create: (context) => RouteGuardBloc(
+          GetIt.I<UserRepositoryAPI>(),
+        )..add(
+            LoadRouteGuardStatus(),
+          ),
+        child: MaterialApp(
+          title: 'Gojo',
+          theme: ThemeData(
+            fontFamily: GoogleFonts.nunito().fontFamily,
+            primarySwatch:
+                Resources.gojoColors.primaryMaterialColor.materialColor,
+          ),
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.generateRoute,
         ),
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: RouteGenerator.generateRoute,
       ),
     ),
   );
